8bae096f4256070b9ecc60435ae492b69cc45f2c
Yahya saad <yahyasaad.me@gmail.com>
2023-12-19 19:27:36 +0530

    Carousel Done

--- conn.js
+++ conn.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
-// var password = encodeURIComponent("#Y1a2h3y4a5");
-// console.log(password);
+var password = encodeURIComponent("#Y1a2h3y4a5");
 mongoose
-  .connect(`mongodb://localhost:27017`)
+  .connect(`mongodb+srv://yahyasaadme:${password}@textapps.er1hajy.mongodb.net/placementapp?retryWrites=true&w=majority`)
   .then(() => console.log("DB Connected"))
   .catch((e) => {
     console.log(e);
